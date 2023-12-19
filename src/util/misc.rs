0c9896c17d61b0d04322990060b07de322bc71f6
Julian Rosse <julian@helixbass.net>
2023-12-19 15:17:36 -0500

    working on adjacent overload signatures

--- src/util/misc.rs
+++ src/util/misc.rs
@@ -1,9 +1,13 @@
 use std::borrow::Cow;
 
-use tree_sitter_lint::{tree_sitter::Node, NodeExt, QueryMatchContext};
+use squalid::CowExt;
+use tree_sitter_lint::{
+    tree_sitter::Node, tree_sitter_grep::SupportedLanguage, NodeExt, QueryMatchContext,
+};
 use tree_sitter_lint_plugin_eslint_builtin::{
     assert_kind,
-    kind::{Identifier, PropertyIdentifier},
+    kind::{ComputedPropertyName, Identifier, PropertyIdentifier},
+    utils::ast_utils::get_static_string_value,
 };
 
 use crate::kind::MethodSignature;
@@ -26,12 +30,31 @@ pub fn get_name_from_member<'a>(
     context: &QueryMatchContext<'a, '_>,
 ) -> MemberName<'a> {
     assert_kind!(member, MethodSignature /*TODO: others*/);
-    let name = member.field("name");
-    match name.kind() {
+    let key = member.field("name");
+    match key.kind() {
         Identifier | PropertyIdentifier => MemberName {
             type_: MemberNameType::Normal,
-            name: name.text(context),
+            name: key.text(context),
         },
+        ComputedPropertyName => MemberName {
+            type_: MemberNameType::Normal,
+            name: key
+                .first_non_comment_named_child(SupportedLanguage::Javascript)
+                .text(context),
+        },
+        tree_sitter_lint_plugin_eslint_builtin::kind::String => {
+            let name = get_static_string_value(key, context).unwrap();
+            // if (requiresQuoting(name)) {
+            //   return {
+            //     type: MemberNameType.Quoted,
+            //     name: `"${name}"`,
+            //   };
+            // }
+            MemberName {
+                type_: MemberNameType::Normal,
+                name,
+            }
+        }
         _ => unimplemented!(),
     }
 }
