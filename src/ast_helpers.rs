be70af8bbf702214c6adbd7242cfa0ab69676eb7
Julian Rosse <julian@helixbass.net>
2023-12-22 12:30:20 -0500

    working on class methods use this

--- src/ast_helpers.rs
+++ src/ast_helpers.rs
@@ -1,12 +1,15 @@
 use squalid::OptionExt;
 use tree_sitter_lint::{tree_sitter::Node, tree_sitter_grep::SupportedLanguage, NodeExt};
 use tree_sitter_lint_plugin_eslint_builtin::{
-    assert_kind, ast_helpers::skip_nodes_of_type, kind::MethodDefinition,
+    assert_kind,
+    ast_helpers::skip_nodes_of_type,
+    kind::{Class, ClassDeclaration, ClassHeritage, MethodDefinition},
 };
 
 use crate::kind::{
-    InterfaceDeclaration, MethodSignature, NestedTypeIdentifier, ObjectType, ParenthesizedType,
-    TypeIdentifier, TypeParameter,
+    AbstractMethodSignature, AccessibilityModifier, ImplementsClause, InterfaceDeclaration,
+    MethodSignature, NestedTypeIdentifier, ObjectType, OverrideModifier, ParenthesizedType,
+    PropertySignature, PublicFieldDefinition, TypeIdentifier, TypeParameter,
 };
 
 pub fn get_is_member_static(node: Node) -> bool {
@@ -42,4 +45,45 @@ pub fn get_is_type_reference(node: Node) -> bool {
         return false;
     }
     true
+}
+
+pub fn get_class_heritage(node: Node) -> Option<Node> {
+    assert_kind!(node, Class | ClassDeclaration);
+
+    node.non_comment_named_children_and_field_names(SupportedLanguage::Javascript)
+        .take_while(|(_, field_name)| *field_name != Some("body"))
+        .find_map(|(node, _)| (node.kind() == ClassHeritage).then_some(node))
+}
+
+pub fn get_class_has_implements_clause(node: Node) -> bool {
+    assert_kind!(node, Class | ClassDeclaration);
+
+    get_class_heritage(node)
+        .matches(|class_heritage| class_heritage.has_child_of_kind(ImplementsClause))
+}
+
+pub fn get_has_override_modifier(node: Node) -> bool {
+    assert_kind!(
+        node,
+        MethodDefinition | PropertySignature | PublicFieldDefinition | MethodSignature
+    );
+
+    node.non_comment_named_children_and_field_names(SupportedLanguage::Javascript)
+        .take_while(|(_, field_name)| *field_name != Some("name"))
+        .any(|(node, _)| node.kind() == OverrideModifier)
+}
+
+pub fn get_accessibility_modifier(node: Node) -> Option<Node> {
+    assert_kind!(
+        node,
+        PublicFieldDefinition
+            | MethodSignature
+            | AbstractMethodSignature
+            | MethodDefinition
+            | PropertySignature
+    );
+
+    node.non_comment_named_children_and_field_names(SupportedLanguage::Javascript)
+        .take_while(|(_, field_name)| *field_name != Some("name"))
+        .find_map(|(node, _)| (node.kind() == AccessibilityModifier).then_some(node))
 }
--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -4,6 +4,7 @@ mod ban_ts_comment;
 mod ban_tslint_comment;
 mod ban_types;
 mod class_literal_property_style;
+mod class_methods_use_this;
 
 pub use adjacent_overload_signatures::adjacent_overload_signatures_rule;
 pub use array_type::array_type_rule;
@@ -11,3 +12,4 @@ pub use ban_ts_comment::ban_ts_comment_rule;
 pub use ban_tslint_comment::ban_tslint_comment_rule;
 pub use ban_types::ban_types_rule;
 pub use class_literal_property_style::class_literal_property_style_rule;
+pub use class_methods_use_this::class_methods_use_this_rule;
